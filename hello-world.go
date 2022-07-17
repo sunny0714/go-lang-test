3bf0c5d8d8e9e6fdce790f3d8bc7e31863528165
sunny0714 <sun2020victory@gmail.com>
2022-07-17 08:39:02 -0700

    variable declaration with initial value

--- hello-world.go
+++ hello-world.go
@@ -8,4 +8,12 @@ func main() {
 	   multi line comment */
 	fmt.Println("Hello, World!")
 	// fmt.Println("Comment to prevent conde execution")
+
+	var student1 string = "John" // type is string
+	var student2 = "Jane"        // type is inferred
+	x := 2                       // type is inferred
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
 }
