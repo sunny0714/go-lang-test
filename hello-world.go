fd7708599abd6d414e4371b80995673f861f8d4d
sunny0714 <sun2020victory@gmail.com>
2022-07-17 10:04:09 -0700

    various variable declaration

--- hello-world.go
+++ hello-world.go
@@ -24,4 +24,39 @@ func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
+
+	// Value assignment after declaration
+	var student3 string
+	student3 = "Jane"
+	fmt.Println(student3)
+
+	// Difference between var and :=
+	// var can be used inside and outside of functions, and variable declaration and value assignment can be done separately
+	// := can only be used inside function, and variable declaration and value assignment must be done in the same line
+
+	// mutiple variable declaration
+	// If use "type" keyword, it's only possible to declare one type of variable per line
+
+	var d, e int = 1, 3
+	var f, g = 6, "Hello"
+	h, i := 7, "World!"
+
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Println(h)
+	fmt.Println(i)
+
+	// varaible declaration in a block
+
+	var (
+		j int
+		k int    = 1
+		l string = "success"
+	)
+
+	fmt.Println(j)
+	fmt.Println(k)
+	fmt.Println(l)
 }
