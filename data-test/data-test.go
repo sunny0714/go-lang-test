de95f66c33e006390f166b814bc9a4c87642f3ea
sunny0714 <sun2020victory@gmail.com>
2022-07-17 18:02:35 -0700

    arrary test

--- data-test/data-test.go
+++ data-test/data-test.go
@@ -28,4 +28,26 @@ func main() {
 
 	fmt.Printf("i has value: %v and type: %T\n", i, i)
 	fmt.Printf("j has value: %v and type: %T\n", j, j)
+
+	// array
+	var arr1 = [3]int{1, 2, 3}      // length is defined
+	arr2 := [...]int{4, 5, 6, 7, 8} // length is inferred
+
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+
+	// arrary initialization
+	arr3 := [5]int{}              // not initialized
+	arr4 := [5]int{1, 2}          // partially initialized
+	arr5 := [5]int{1, 2, 3, 4, 5} // fully initialized
+	arr6 := [5]int{1: 10, 2: 40}  // initialize only specific elements
+
+	fmt.Println(arr3)
+	fmt.Println(arr4)
+	fmt.Println(arr5)
+	fmt.Println(arr6)
+
+	// lenght of arrary
+	fmt.Println(len(arr6))
+
 }
